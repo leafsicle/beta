df5a811cb741a5880565fad0c1dc974ec058ce31
Matthew Cooke <cookem529@gmail.com>
2019-03-04 20:28:06 -0500

    making a calc. knock off the rust a bit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,3 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+const main = () => {}
 
 document.addEventListener('DOMContentLoaded', main)
